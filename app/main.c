fe28abaf5444503aa9b27292d1590bc195625a95
Sedrak Dalaloyan <sedrakpc@users.noreply.github.com>
2024-12-19 01:02:44 -0500

    added repl loop

--- app/main.c
+++ app/main.c
@@ -6,22 +6,22 @@ int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-   printf("$ ");
-
-  // Wait for user input
   while(1) {
-      char input[100];
-      fgets(input, 100, stdin);
-      int len = strlen(input);
-      if (len > 0 && input[len - 1] == '\n') {
-          input[len - 1] = '\0';
-      }
-      if (isValidCommand(input)) {
-          printf("Command is valid\n");
-      } else {
-          printf("%s: command not found\n", input);
-      }
+    // Uncomment this block to pass the first stage
+    printf("$ ");
+
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+    int len = strlen(input);
+    if (len > 0 && input[len - 1] == '\n') {
+      input[len - 1] = '\0';
+    }
+    if (isValidCommand(input)) {
+      printf("Command is valid\n");
+    } else {
+      printf("%s: command not found\n", input);
+    }
   }
   return 0;
 }
