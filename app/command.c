a0bd3e5ed3727dada364168ddbbb3f2e9502503d
Sedrak Dalaloyan <sedrakpc@users.noreply.github.com>
2024-12-19 14:42:49 -0500

    implemented echo command

--- app/command.c
+++ app/command.c
@@ -3,8 +3,9 @@
 #include "string.h"
 #include "../include/command.h"
 
-char COMMANDS[1][10] = {
-        "exit"
+char COMMANDS[2][10] = {
+        "exit",
+        "echo"
 };
 int NUMBER_OF_SUPPORTED_COMMANDS = sizeof(COMMANDS) / sizeof(COMMANDS[0]);
 
@@ -18,14 +19,35 @@ int isValidCommand(char* command) {
     return 0;
 }
 
-int processCommand(char* command, char* args[], int argsCount) {
+int validateArguments(char* command, char* args[], int argsCount) {
     if (strcasecmp(command, "exit") == 0) {
         if (argsCount < 2) {
             printf("invalid number of arguments\n");
-        } else {
+            return 0;
+        }
+    }
+    if (strcasecmp(command, "echo") == 0) {
+        if (argsCount < 2) {
+            printf("invalid number of arguments\n");
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int processCommand(char* command, char* args[], int argsCount) {
+    if (strcasecmp(command, "exit") == 0) {
+        if (validateArguments(command, args, argsCount)) {
             exit(atoi(args[1]));
         }
-        return 1;
+    } else if (strcasecmp(command, "echo") == 0) {
+        if (validateArguments(command, args, argsCount)) {
+            for (int i = 1; i < argsCount; i++) {
+                printf("%s ", args[i]);
+            }
+            printf("\n");
+            return 1;
+        }
     }
     return 0;
 }
